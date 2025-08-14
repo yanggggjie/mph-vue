f2f3389f8fc267277a0f21894d2ce462e6391def
jeoyang <jeoyang@tencent.com>
2025-08-14 12:57:52 +0800

    fix: commonjs support for lower vscoder version

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import vscode from '@tomjs/vite-plugin-vscode';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag: string) => tag.startsWith('vscode-'),
+export default defineConfig(async () => {
+  // 动态导入 ESM 包以在 CommonJS 环境中使用
+  const { default: tailwindcss } = await import('@tailwindcss/vite');
+  
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag: string) => tag.startsWith('vscode-'),
+          },
         },
-      },
-    }),
-    vscode({
-      recommended: true,
-      webview: {
-        // csp: '<meta http-equiv="Content-Security-Policy" />',
-      },
-    }),
-    tailwindcss(),
-  ],
+      }),
+      vscode({
+        recommended: true,
+        webview: {
+          // csp: '<meta http-equiv="Content-Security-Policy" />',
+        },
+      }),
+      tailwindcss(),
+    ],
+  };
 });
